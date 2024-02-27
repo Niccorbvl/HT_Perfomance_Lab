7133930a38c350bd92a59b5d1d2fa530a9bda70c
Niccorbvl <161502076+Niccorbvl@users.noreply.github.com>
2024-02-27 22:22:07 +0400

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-n, m = map(int, input().split())
-FirstElement = 1
-FirstIteration = True
-while (FirstElement != 1) or FirstIteration:
-    FirstIteration = False
-    print(FirstElement, end='')
-    FirstElement = (FirstElement + m - 2) % n + 1
